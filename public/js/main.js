eb9dee5fb54ca1a5f5c6e4e6820dec0189b6a344
Maxine Hood <mhood@wellesley.edu>
2017-04-22 17:16:32 -0400

    fun added

--- public/js/main.js
+++ public/js/main.js
@@ -3,13 +3,13 @@ function myFunction() {
   // use jQuery ($ is shorthand) to find the div on the page and then change the html
   // jQuery can do a lot of crazy stuff so make sure to google around to find out more
 
-  $("#demo").html("NEWWW PARAGRAPH #javascript #fire");
+  $("#demo").html("nah you aint cool");
 
   // 'img-circle' is a bootstrap thing! Check out more here: http://getbootstrap.com/css/
-  $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
+  $("#cool-image").append(`<img class="img-circle" src="/images/cool.gif" />`);
 }
 
 function button2() {
-  $("#demo").html("!!!!!!!!")
+  $("#demo").html("!!!!!!!!youre cool!!!!!!")
   $("#doge-image").append(`<img class="img-circle" src="/images/wowdoge.jpeg" />`);
 }
